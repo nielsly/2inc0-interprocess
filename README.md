6ffd435d7943ec7d2e1a971fe454696a67beb521
Niels Gorter <nierls@live.nl>
2020-11-24 13:14:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Interprocess communication
+
+2INC0 Assignment 1 <https://canvas.tue.nl/courses/14805/pages/interprocess-communication>
---
+++ farmer.c
@@ -0,0 +1,48 @@
+/* 
+ * Operating Systems <2INCO> Practical Assignment
+ * Interprocess Communication
+ *
+ * STUDENT_NAME_1 (STUDENT_NR_1)
+ * STUDENT_NAME_2 (STUDENT_NR_2)
+ *
+ * Grading:
+ * Students who hand in clean code that fully satisfies the minimum requirements will get an 8. 
+ * Extra steps can lead to higher marks because we want students to take the initiative. 
+ * These extra steps must be agreed with the tutor before delivery.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>    
+#include <unistd.h>         // for execlp
+#include <mqueue.h>         // for mq
+
+#include "settings.h"
+#include "common.h"
+
+
+int main (int argc, char * argv[])
+{
+    if (argc != 1)
+    {
+        fprintf (stderr, "%s: invalid arguments\n", argv[0]);
+    }
+        
+    // TODO:
+    //  * create the message queues (see message_queue_test() in interprocess_basic.c)
+    //  * create the child processes (see process_test() and message_queue_test())
+    //  * do the farming
+    //  * wait until the chilren have been stopped (see process_test())
+    //  * clean up the message queues (see message_queue_test())
+
+    // Important notice: make sure that the names of the message queues contain your
+    // student name and the process id (to ensure uniqueness during testing)
+    
+    return (0);
+}
+
